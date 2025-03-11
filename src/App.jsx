d7686d48ed3721ac65bad48f8ddd61c4927c2be8
kusi <appaukusi05@gmail.com>
2025-03-11 17:57:01 +0000

    add: input for personal details

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,35 @@
+import { useState } from "react";
 
+import PersonalDetails from "./components/PersonalDetails";
+
+import { initPersonalDetails } from "./components/initData";
+
+export default function App() {
+  const [personalDetails, setPersonalDetails] = useState(initPersonalDetails);
+  const [activeInputArea, setIActiveInputArea] = useState(0);
+  return (
+    <section>
+      <InputArea
+        sectionName={"Personal Details"}
+        isActive={activeInputArea == 0}
+        onShow={() => setIActiveInputArea(0)}
+      >
+        <PersonalDetails 
+            data={personalDetails}
+            setData={setPersonalDetails}
+        />
+      </InputArea>
+    </section>
+  );
+}
+
+function InputArea({ className, sectionName, children, isActive, onShow }) {
+  return (
+    <section className={className}>
+      <fieldset>
+        <h2>{sectionName}</h2>
+        {isActive ? { children } : <button onClick={onShow}>+</button>}
+      </fieldset>
+    </section>
+  );
+}
---
+++ src/components/PersonalDetails.jsx
@@ -0,0 +1,25 @@
+import { Input } from "./utils";
+
+export default function PersonalDetails({data, setData}) {
+  function handleChange(e, name){
+    setData({...data, [name]: e.target.valuw})
+  }
+  return (
+    <>
+      <Input
+        lable={"First Name"}
+        name={"firstName"}
+        data={data.firstName}
+        handleChange={handleChange}
+        placeholder={"Kofi"}
+      />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+    </>
+  );
+}
